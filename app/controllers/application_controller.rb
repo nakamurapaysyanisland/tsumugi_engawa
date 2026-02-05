42cadd13bc652d53cfe389f292e3941de0915b20
YukaNakamura <242622452+nakamurapaysyanisland@users.noreply.github.com>
2026-02-05 05:50:51 +0000

    params記述

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+ before_action :configure_permitted_parameters, if: :devise_controller?
+
+
+ def after_sign_put_path_for(resource)
+    root_path
+ end
+ 
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :nickname])
+  end
+
 end
