bd2f33b6b8f3e4e0bdd2d023b9b673be5c2cca5b
YukaNakamura <242622452+nakamurapaysyanisland@users.noreply.github.com>
2026-02-08 12:06:39 +0000

    ユーザー情報編集画面

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,28 +1,53 @@
 class PostsController < ApplicationController
   def new
-    @posts = Post.new
+    @post = Post.new
   end
 
   def index
+
+    @posts = Post.includes(:user).all.order(created_at: :desc)
+  @user = current_user
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    
+    if @post.save
+      flash[:notice] = "投稿に成功しました。"
+      redirect_to post_path(@post)
+    else
+      render :new
+    end
   end
 
   def edit
+    @post = Post.find(params[:id])
+    
   end
 
   def update
+     @post = Post.find(params[:id])
+    if @post.update(post_params)
+      flash[:notice] = "更新に成功しました。"
+      redirect_to user_path(current_user)
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to user_path(current_user)
   end
 
   private
-  def posts_params
-    params.require(:posts).permit(:title, :body)
+  def post_params
+    params.require(:post).permit(:title, :body, :category_id)
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,7 @@ class Post < ApplicationRecord
     validates :body, presence: true
     validates :user_id, presence: true
     validates :category, presence: true
+
+    belongs_to :category
+    belongs_to :user
 end
