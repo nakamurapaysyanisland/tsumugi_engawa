85f608d9b05c9bd89f587eaa59fa03bccb31eb43
YukaNakamura <242622452+nakamurapaysyanisland@users.noreply.github.com>
2026-02-11 11:26:25 +0000

    guest登録機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,29 @@
 Rails.application.routes.draw do
-  devise_for :user
-  resources :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
+  }
+
+  resources :users, only: [:index, :show, :edit, :update] do
+    member do
+      get :edit_upgrade
+      patch :upgrade
+      patch :withdraw
+    end
+  end
+
+  devise_scope :user do
+    post 'users/guest_sign_up', to: 'users/registrations#guest_create'
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+  end
+
+  get 'guest_signup', to: 'users#guest_signup'
+  post 'guest_signup', to: 'users#create_guest'
+
+  
+
   resources :posts
- 
+  get 'mypage' => 'users#mypage', as: 'mypage'
   root to: 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
