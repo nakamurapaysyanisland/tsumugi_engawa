26232dd8f9c9624d084e6f84f1327a14efd28317
Ubuntu <ubuntu@ip-172-31-33-129.ap-northeast-1.compute.internal>
2026-02-20 13:03:51 +0000

    user_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,11 +9,11 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = current_user
+    @user = User.find(params[:id])
   end
 
   def update
-    @user = current_user
+    @user = User.find(params[:id])
     if @user.update(user_params)
        bypass_sign_in(@user) 
       flash[:notice] = "ユーザー情報を更新しました。"
@@ -57,10 +57,9 @@ class UsersController < ApplicationController
     @user.email = "guest_#{Time.now.to_i}#{rand(1000)}@example.com"
     @user.password = SecureRandom.alphanumeric(10)
     if @user.save
-      sign_in @user
+      session[:user_id] = @user.id
       redirect_to mypage_path, notice: "ゲストユーザーとしてログインしました。"
     else
-      logger.error @user.errors.full_messages
       render :new_guest
     end
   end
