0992b079acff4824dffd30111b0a144560472e74
Ubuntu <ubuntu@ip-172-31-33-129.ap-northeast-1.compute.internal>
2026-02-22 12:06:26 +0000

    seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Category.find_or_create_by(name: "給料")
+Category.find_or_create_by(name: "衛生管理")
