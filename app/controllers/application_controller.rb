e01ff51925c55db97cbb2439fcaae05038e0df79
nakamurapaysyanisland <242622452+nakamurapaysyanisland@users.noreply.github.com>
2026-02-23 13:17:27 +0000

    1stデプロイ+テスト完了

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 class ApplicationController < ActionController::Base
  before_action :configure_permitted_parameters, if: :devise_controller?
-
- def after_sign_in_put_path_for(resource)
-    user_path(resource)
- end
+  before_action :authenticate_user!
+ def after_sign_in_path_for(resource)
+    mypage_path(resource)
+   end
 
  def after_sign_out_path_for(resource)
     root_path
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,7 +7,7 @@ class PostsController < ApplicationController
 
   def index
     @posts = Post.includes(:user).all.order(created_at: :desc)
-  @user = current_user
+      @user = current_user
   end
 
   def show
