9073de1bb26e10fc861864c9b440a28626166b26
nakamurapaysyanisland <242622452+nakamurapaysyanisland@users.noreply.github.com>
2026-03-09 05:00:13 +0000

    実装フェーズ２完了

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -11,13 +11,13 @@ def after_sign_in_path_for(resource)
   end
  end
 
- def after_sign_out_path_for(resource)
-  if resource == :admin
+def after_sign_out_path_for(resource_or_scope)
+  if resource_or_scope == :admin
     new_admin_session_path
   else
     root_path
   end
- end
+end
  def create
     build_resource(sign_up_params)
     resource.save
@@ -39,5 +39,8 @@ def after_sign_in_path_for(resource)
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :nickname])
+  end
+  def after_sign_up_path_for(resource)
+    mypage_path
   end
 end
