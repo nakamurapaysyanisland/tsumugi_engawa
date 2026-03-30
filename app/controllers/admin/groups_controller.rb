882d3d355a9980f7ba35db32360da687555ac6f2
nakamurapaysyanisland <242622452+nakamurapaysyanisland@users.noreply.github.com>
2026-03-30 13:55:46 +0000

    Refactor: Group取得処理の重複を削除

--- app/controllers/admin/groups_controller.rb
+++ app/controllers/admin/groups_controller.rb
@@ -1,20 +1,22 @@
 class Admin::GroupsController < Admin::BaseController
+  
   def index
     @groups = Group.all.page(params[:page]).per(17)
   end
 
   def show
     @group = Group.find(params[:id])
-    @group = Group.find_by(id: params[:id])
     @posts = @group.posts.page(params[:page]).per(10)
-    
   end
+
   def destroy
-  @group = Group.find(params[:id])
-  if @group.destroy
-    redirect_to admin_groups_path, notice: "グループ「#{@group.name}」を削除しました。"
-  else
-    redirect_to admin_group_path(@group), alert: "削除に失敗しました。"
+    @group = Group.find(params[:id])
+
+    if @group.destroy
+      redirect_to admin_groups_path, notice: "グループ「#{@group.name}」を削除しました。"
+    else
+      redirect_to admin_group_path(@group), alert: "削除に失敗しました。"
+    end
   end
-end
+  
 end
