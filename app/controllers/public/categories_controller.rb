8de4d9b251e56f88533812d1411d3fa92dae39e8
nakamurapaysyanisland <242622452+nakamurapaysyanisland@users.noreply.github.com>
2026-03-31 08:08:48 +0000

    refactor: CategoriesControllerを削除し、検索ビューへ統合

--- app/controllers/public/categories_controller.rb
+++ app/controllers/public/categories_controller.rb
@@ -1,11 +1,4 @@
 class Public::CategoriesController < ApplicationController
-  skip_before_action :configure_authentication, only: [:index], raise: false
-  def show
-    @category = Category.find(params[:id])
-    @post = Category.page(params[:page]).per(10)
-  end
+  
 
-  def index
-    @category = Category.all
-  end
 end
