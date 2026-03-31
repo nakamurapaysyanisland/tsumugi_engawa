914736fb0c280475d017e607c69475bb557073fc
nakamurapaysyanisland <242622452+nakamurapaysyanisland@users.noreply.github.com>
2026-03-31 11:17:10 +0000

    refactor: GroupUsersのcreateアクションの重複したacceptedを削除

--- app/controllers/public/group_users_controller.rb
+++ app/controllers/public/group_users_controller.rb
@@ -1,11 +1,11 @@
 class Public::GroupUsersController < ApplicationController
     before_action :authenticate_user!
+
     def create
         @group = Group.find(params[:group_id])
         @group_user = current_user.group_users.find_or_initialize_by(group_id: @group.id)
         if @group_user.save
-        @group.create_notification_group_approved!(current_user, current_user.id)
-        @group.create_notification_group_join!(current_user)
+            @group.create_notification_group_join!(current_user)
         end
 
         respond_to do |format|
@@ -15,17 +15,15 @@ class Public::GroupUsersController < ApplicationController
     end
 
     def destroy
-  @group_user = GroupUser.find(params[:id])
-  @group = @group_user.group 
-  
-  @group_user.destroy
+        @group_user = GroupUser.find(params[:id])
+        @group = @group_user.group 
+        @group_user.destroy
 
-  respond_to do |format|
-
-    format.html { redirect_to group_path(@group), notice: "参加を取り消しました。" }
-    format.js
-  end
-end
+        respond_to do |format|
+            format.html { redirect_to group_path(@group), notice: "参加を取り消しました。" }
+            format.js
+        end
+    end
 
     def index
     @group = Group.find(params[:group_id])
@@ -33,13 +31,13 @@ end
    end
 
    def update
-    @group = Group.find(params[:group_id])
-    @group_user = GroupUser.find(params[:id])
-    if @group_user.update(status: 1)
-        @group.create_notification_group_approved!(current_user, @group_user.user_id)
-        redirect_to group_group_users_path(@group), notice: "グループへの参加を承認しました。"
-    else
-        redirect_to group_group_users_path(@group), notice: "グループへの承認に失敗しました。"
-    end
+        @group = Group.find(params[:group_id])
+        @group_user = GroupUser.find(params[:id])
+        if @group_user.update(status: 'accepted')
+            @group.create_notification_group_accepted!(current_user, @group_user.user_id)
+            redirect_to group_group_users_path(@group), notice: "グループへの参加を承認しました。"
+        else
+            redirect_to group_group_users_path(@group), notice: "グループへの承認に失敗しました。"
+        end
     end
 end
