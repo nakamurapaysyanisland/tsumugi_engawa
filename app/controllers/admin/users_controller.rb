9bb7869156332341592c415fd9c053b96379222c
nakamurapaysyanisland <242622452+nakamurapaysyanisland@users.noreply.github.com>
2026-03-31 07:20:44 +0000

    refactor: 繰り返されていたUser.findをset_userメソッドに移動

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,16 +1,20 @@
 class Admin::UsersController < Admin::BaseController
     layout 'admin'
     before_action :authenticate_admin!
-
+    before_action :set_user, only: [:show, :withdraw]
 
      def show
-        @user = User.find(params[:id])
         @posts = @user.posts.page(params[:page]).per(12)
     end
+    
     def withdraw
-        @user = User.find(params[:id])
         @user.update(status: :withdrawn)
         redirect_to admin_dashboards_path, notice: "#{@user.nickname}様を退会させました。"
     end
    
+    private
+
+    def set_user
+        @user = User.find(params[:id])
+    end
 end
